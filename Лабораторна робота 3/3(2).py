1b0683dea065a707138d8b7a07b11faf7f8db6b0
Oleksandr Voznyi <voznii978@gmail.com>
2022-11-19 00:28:42 +0200

    lb3.2

--- Лабораторна робота 3/3(2).py
+++ Лабораторна робота 3/3(2).py
@@ -5,17 +5,21 @@ from scipy.misc import derivative
 def f(x):
     return 4.5*pow(x,4) - 4*pow(x,3) + 1.5*pow(x,2) - 2*x - 7
 
-def nuton(a,b,eps):
-    df2 = derivative(f, b, n=1)
-    if (f(b)*df2>0):
-        xi = b
+def kombo(a,b,eps):
+    if (derivative(f, a, n = 1)*derivative(f, a, n =2)>0):
+        a0 = 0
+        b0 = 0
     else:
-        xi = a
-    df = derivative(f, xi, n=1)
-    xi_1 = xi = f(xi)/df
-    while (abs(xi_1 - xi)>eps):
-        xi = xi_1
-        xi_1 = xi - f(xi)/df
-    return print(f"\nSolving the equation by Newton*s method x = {round(xi_1, 5)}\n")
+        a0 = b
+        b0 = a
+    ai = a0
+    bi = b0
+    while abs(ai-bi)>eps:
+        ai_1 = ai - f(ai)*(bi - ai)/(f(bi) - f(ai))
+        bi_1 = bi - f(bi)/derivative(f, bi, n = 1)
+        ai_1 = ai_1
+        bi_1 = bi_1
+    x = (ai_1+bi_1)/2
+    return print(f"\nSolving the equation by Newton*s method x = {round(x, 5)}\n")
 
-nuton(-2, -1/2, 0.0001)
+kombo(-2, -1/2, 0.0001)
